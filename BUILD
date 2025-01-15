19dc9b6ce5d95cd54ff392381bdabfd625ad502a
Nicolae Dicu <nicolae.dicu.nd@gmail.com>
2025-01-15 09:53:33 +0200

    initial cpp and bazel file structure

---
+++ BUILD
@@ -0,0 +1,4 @@
+# BUILD
+load("@bazel_tools//tools/build_defs/pkg:pkg.bzl", "pkg_tar")
+
+# Add top-level configurations or targets if needed
---
+++ math/math_functions.cpp
@@ -0,0 +1,15 @@
+#include "math_functions.h"
+
+namespace math {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+}
