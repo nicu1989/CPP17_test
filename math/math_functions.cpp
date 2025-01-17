510c55737f700257c7187fe5bcaeb1e38b12bcee
Nicolae Dicu <nicolae.dicu.nd@gmail.com>
2025-01-17 13:08:01 +0100

    Create a MISRA error

--- math/math_functions.cpp
+++ math/math_functions.cpp
@@ -1,8 +1,8 @@
 #include "math_functions.h"
-
+#define ADD(a,b) (a+b)
 namespace math {
     int add(int a, int b) {
-        return a + b;
+        return ADD(a,b);
     }
 
     int subtract(int a, int b) {
